3e7d95c433619d6cf6fffc499e5b24bbd495edae
Christopher <tsod2000@gmail.com>
2021-11-19 10:30:24 -0500

    Second Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
+import { HomeComponent } from './home/home.component';
+import { SourcesComponent } from './sources/sources.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', 
+  component: HomeComponent},
+  {path: 'headlines',
+  component: HeadlinesComponent},
+  {path: 'sources',
+  component: SourcesComponent},
+  {path: 'favorites',
+  component: FavoritesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 
 
 
 @NgModule({
   declarations: [],         // For buttons
-  imports: [ CommonModule, MatButtonModule], 
+  imports: [ CommonModule, MatButtonModule, MatToolbarModule, MatCardModule], 
   // Add exports so that our module dependencies are available everywhere inside of our application
-  exports: [MatButtonModule]
+  exports: [MatButtonModule, MatToolbarModule, MatCardModule]
 })
 export class MaterialModule { }
